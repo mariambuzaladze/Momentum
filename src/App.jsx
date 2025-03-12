c3341c0eb37b378c065d8a1be588db2255d1150f
mariambuzaladze <mariambuzaladze10@example.come>
2025-03-12 22:07:51 +0400

    feat: add pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Task from "./pages/Task";
+import AddTask from "./pages/AddTask";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/task" element={<Task />} />
+        <Route path="/addTask" element={<AddTask />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
