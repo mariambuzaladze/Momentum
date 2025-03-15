0ce8c970302b749d74e73c08efa5ac01a9db7bbf
mariambuzaladze <mariambuzaladze10@example.come>
2025-03-15 22:12:26 +0400

    wip: add task overview component, working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,112 @@
+import { useEffect, useState } from "react";
+import TaksOverview from "./components/TaskOverview";
+
 export default function Home() {
-  return <></>;
+  const [tasks, setTasks] = useState([]);
+  const [statuses, setStatusess] = useState([]);
+
+  async function getTasks() {
+    try {
+      const response = await fetch(
+        "https://momentum.redberryinternship.ge/api/tasks",
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer 9e6c7cca-7d41-4f5b-8c6d-585a9921a547",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Tasks failed:", error);
+    }
+  }
+
+  async function getStatuses() {
+    try {
+      const response = await fetch(
+        "https://momentum.redberryinternship.ge/api/statuses",
+        {
+          method: "GET",
+          headers: {},
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Statuses failed:", error);
+    }
+  }
+
+  useEffect(() => {
+    getTasks()
+      .then((data) => setTasks(data))
+      .catch((error) => console.error("Error:", error));
+
+    getStatuses()
+      .then((data) => setStatusess(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <main className="flex flex-col pt-5 px-[100px] gap-[55px]">
+      <h1 className="text-[34px] font-semibold">დავალებების გვერდი</h1>
+
+      <section className="flex gap-[45px] px-4 py-2 w-fit rounded-[10px] border border-[#dee2e6]">
+        <div className="flex gap-3 items-center">
+          <p>დეპარტამენტი</p>
+          <img src="Shape (1).svg" alt="arrow down logo" />
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <p>პრიპრიტეტი</p>
+          <img src="Shape (1).svg" alt="arrow down logo" />
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <p>თანამშრომელი</p>
+          <img src="Shape (1).svg" alt="arrow down logo" />
+        </div>
+      </section>
+
+      <section>
+        <div className="flex justify-between">
+          {statuses.map((status) => {
+            const statusColors = {
+              1: "bg-[#f7bc30]",
+              2: "bg-[#fb5607]",
+              3: "bg-[#ff006e]",
+              4: "bg-[#3a86ff]",
+            };
+
+            return (
+              <div
+                key={status.id}
+                className={`border rounded-[10px] py-4 w-[380px] flex justify-center items-center text-white text-[20px] ${
+                  statusColors[status.id] || "bg-gray-400"
+                }`}
+              >
+                {status.name}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+
+      <section>
+        {tasks.map((task) => {
+          return <TaksOverview key={task.id} task={task} />;
+        })}
+      </section>
+    </main>
+  );
 }
---
+++ src/pages/components/TaskOverview.jsx
@@ -0,0 +1,92 @@
+import { format } from "date-fns";
+import { ka } from "date-fns/locale";
+
+export default function TaksOverview({ task }) {
+  const formatDueDate = (dateString) => {
+    return format(new Date(dateString), "dd MMM yy", { locale: ka });
+  };
+
+  const getPriorityStyles = (priorityName) => {
+    switch (priorityName) {
+      case "დაბალი":
+        return {
+          textColor: "text-[#08a508]",
+          borderColor: "border-[#08a508]",
+        };
+      case "საშუალო":
+        return {
+          textColor: "text-[#ffbe0b]",
+          borderColor: "border-[#ffbe0b]",
+        };
+      case "მაღალი":
+        return {
+          textColor: "text-[#fa4d4d]",
+          borderColor: "border-[#fa4d4d]",
+        };
+    }
+  };
+
+  const { textColor, borderColor } = getPriorityStyles(task.priority.name);
+
+  const getDepartmentBgColor = (departmentName) => {
+    switch (departmentName) {
+      case "ადმინისტრაციის დეპარტამენტი":
+        return "bg-[#FF66A8]";
+      case "ადამიანური რესურსების დეპარტამენტი":
+        return "bg-[#FD9A6A]";
+      case "ფინანსების დეპარტამენტი":
+        return "bg-[#89B6FF]";
+      case "გაყიდვები და მარკეტინგის დეპარტამენტი":
+        return "bg-[#FFD86D]";
+      case "ლოჯოსტიკის დეპარტამენტი":
+        return "bg-[#89B6FF]";
+      case "ტექნოლოგიების დეპარტამენტი":
+        return "bg-[#FD9A6A]";
+      case "მედიის დეპარტამენტი":
+        return "bg-[#FFD86D]";
+    }
+  };
+
+  const departmentBgColor = getDepartmentBgColor(task.department.name);
+
+  return (
+    <div className="flex flex-col p-5 gap-7 w-[380px] rounded-[15px] border border-[#f7bc30]">
+      <div className="flex justify-between">
+        <div className="flex gap-2 items-center">
+          <div
+            className={`flex gap-1 items-center rounded-[5px] border ${borderColor} border-opacity-50 p-1`}
+          >
+            <img src={task.priority.icon} alt="priority icon" />
+            <p className={textColor}>{task.priority.name}</p>
+          </div>
+
+          <div
+            className={`w-[88px] truncate px-[9px] py-[5px] rounded-[15px] ${departmentBgColor} text-white`}
+          >
+            {task.department.name}
+          </div>
+        </div>
+
+        <p className="text-[#212529]">{formatDueDate(task.due_date)}</p>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <p className="font-medium text-[#212529]">{task.name}</p>
+        <p className="text-[#343a40]">{task.description}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <img
+          src={task.employee.avatar}
+          alt="employee avatar"
+          className="w-[31px] h-[31px] rounded-full"
+        />
+
+        <div className="flex gap-1 items-center">
+          <img src="Comments.svg" alt="comments icon" />
+          <p>{task.total_comments}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
